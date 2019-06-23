5400e7ad68c52b27160c4f200f5d0d6c845ed008
Tim Herold <herold.tim@gmail.com>
2019-06-23 12:17:34 +0200

    _

--- libsodium.rb
+++ libsodium.rb
@@ -1,5 +1,25 @@
 # libsodium.rb
+
+require 'ffi'
+
 module LibSodium
   extend FFI::Library
   ffi_lib ['libsodium', 'libsodium23', 'libsodium.so', 'libsodium.so.23', 'libsodium.so.23.1.0']
+  
+  begin
+    RBNACL_AVAILABLE = if ENV['DISCORDRB_NONACL']
+                       false
+                     else
+                       require 'rbnacl'
+                       puts "libsodium available!"
+					   true
+                     end
+  rescue LoadError
+    puts "libsodium not available! You can continue to use discordrb as normal but voice support won't work.
+        Read https://github.com/meew0/discordrb/wiki/Installing-libsodium for more details."
+    RBNACL_AVAILABLE = false
+  else
+    puts "libsodium available!"
+  end
+
 end
